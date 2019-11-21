e1626bb1525bf8cc89d24d7751faaa163fca2876
ttnny <ttnny@users.noreply.github.com>
2019-11-20 22:46:27 -0800

    [#2] Building a prototype

--- main.go
+++ main.go
@@ -1,18 +1,52 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/google/go-github/github"
+	"github.com/gorilla/mux"
 	"net/http"
 	"os"
 )
 
 func main() {
-	http.HandleFunc("/", index)
+	r := mux.NewRouter()
 
-	http.HandleFunc("/api/glc", ChartHandleFunc)
-	http.HandleFunc("/api/glc/", ContributionsHandleFunc)
+	// Route: index
+	r.HandleFunc("/", indexHandleFunc).Methods("GET")
 
-	http.ListenAndServe(port(), nil)
+	// Route: chart
+	r.HandleFunc("/api/github-lcg/chart/{username}", chartHandleFunc).Methods("GET")
+
+	// Route: contributions
+	r.HandleFunc("/api/github-lcg/contributions/{username}", contributionsHandleFunc).Methods("GET")
+
+	// Let's start
+	http.ListenAndServe(port(), r)
+}
+
+func indexHandleFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there! Welcome to GitHub Languages Chart Generator.")
+}
+
+func chartHandleFunc(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	vars := mux.Vars(r)
+	username := vars["username"]
+
+	// Construct a new GitHub client
+	gc := github.NewClient(nil)
+	result, _, _ := gc.Repositories.ListLanguages(ctx, username, "go")
+
+	fmt.Fprintf(w, "Languages used in the repo 'go' from %s", username)
+	fmt.Println(result)
+}
+
+func contributionsHandleFunc(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	username := vars["username"]
+
+	fmt.Fprintf(w, "You've requested the book: %s on page %s\n", username)
 }
 
 func port() string {
@@ -23,9 +57,4 @@ func port() string {
 	}
 
 	return ":" + port
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Hi there! This is GitHub Languages Chart Generator.")
 }
