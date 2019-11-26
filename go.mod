936040f928f73ff20d6f25e1109f35e3ed683c6d
ttnny <ttnny@users.noreply.github.com>
2019-11-25 22:52:25 -0800

    [#2] Create a list of repos

--- go.mod
+++ go.mod
@@ -4,9 +4,9 @@ go 1.13
 
 require (
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
-	github.com/google/go-github v17.0.0+incompatible
-	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/google/go-github/v28 v28.1.1 // indirect
 	github.com/gorilla/mux v1.7.3
 	github.com/wcharczuk/go-chart v2.0.1+incompatible // indirect
+	golang.org/x/crypto v0.0.0-20191122220453-ac88ee75c92c // indirect
 	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
 )
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/google/go-github/github"
+	"github.com/google/go-github/v28/github"
 	"github.com/gorilla/mux"
 	"net/http"
 	"os"
@@ -16,32 +16,72 @@ func main() {
 	r.HandleFunc("/", indexHandleFunc).Methods("GET")
 
 	// Route: chart
-	r.HandleFunc("/api/github-lcg/chart/{username}", chartHandleFunc).Methods("GET")
+	r.HandleFunc("/app/github-lcg/api/chart/{username}", chartHandleFunc).Methods("GET")
 
 	// Route: contributions
-	r.HandleFunc("/api/github-lcg/contributions/{username}", contributionsHandleFunc).Methods("GET")
+	r.HandleFunc("/app/github-lcg/api/contributions/{username}", contributionsHandleFunc).Methods("GET")
 
 	// Let's start
-	http.ListenAndServe(port(), r)
+	err := http.ListenAndServe(port(), r)
+	if err != nil {
+		fmt.Printf("Errors: %v\n", err)
+	}
 }
 
-func indexHandleFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there! Welcome to GitHub Languages Chart Generator.")
+// Route: index/home
+func indexHandleFunc(w http.ResponseWriter, _ *http.Request) {
+	_, err := fmt.Fprintf(w, "Hi there! Welcome to GitHub Languages Chart Generator.")
+	if err != nil {
+		fmt.Printf("Errors: %v\n", err)
+	}
 }
 
+// Route: generates chart
 func chartHandleFunc(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
 	vars := mux.Vars(r)
 	username := vars["username"]
 
-	// Construct a new GitHub client
-	gc := github.NewClient(nil)
-	result, _, _ := gc.Repositories.ListLanguages(ctx, username, "go")
+	client := github.NewClient(nil)
 
-	fmt.Fprintf(w, "Languages used in the repo 'go' from %s", username)
-	fmt.Println(result)
+	// Get a slice of
+	repos, _, err := client.Repositories.List(ctx, username, nil)
+
+	// Address API rate limit and other errors
+	if err != nil {
+		fmt.Printf("Errors: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Create a list of repos
+	var list []string
+	for _, repo := range repos {
+		list = append(list, *repo.Name)
+	}
+
+	// Get languages info for each repo
+	var langs map[string]int
+	for _, repo := range list {
+		lang, _, err := client.Repositories.ListLanguages(ctx, username, repo)
+		if err != nil {
+			fmt.Printf("Errors: %v\n", err)
+		}
+
+		for k, v := range lang {
+			if value, found := langs[k]; found { // if exists, add up value only
+				langs[k] = v + value
+			} else {
+				langs[k] = v
+			}
+		}
+	}
+
+	for key, value := range langs { // Order not specified
+		fmt.Fprintln(w, key, value)
+	}
 }
 
+// Route: generates graph of GitHub contributions
 func contributionsHandleFunc(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	username := vars["username"]
@@ -49,6 +89,7 @@ func contributionsHandleFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "You've requested the book: %s on page %s\n", username)
 }
 
+// Get/set the default port
 func port() string {
 	port := os.Getenv("PORT")
 
