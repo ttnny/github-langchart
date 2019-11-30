66cb229f334832992cf0e74b5a4ea7080a03b766
ttnny <ttnny@users.noreply.github.com>
2019-11-30 03:48:46 -0800

    [#8] Complete creating GitHub Language Chart (Bar & Pie). Closes #8

--- demo.go
+++ demo.go
@@ -1,19 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"net/http"
 )
 
+type LangChart struct {
+	Langs     []string
+	Stats     []int
+	ChartType string
+}
+
 // Route: (Demo) Handle Index
 func demoIndexHandleFunc(w http.ResponseWriter, _ *http.Request) {
-
 }
 
 // Route: (Demo) Handle Display GitHub LangStats
 func demoLangStatsHandleFunc(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/langstats.gohtml"))
+	tmpl := template.Must(template.ParseFiles("templates/langstats.gohtml", "templates/langchart.gohtml"))
 
 	if r.Method == http.MethodGet {
 		tmpl.Execute(w, nil)
@@ -21,19 +25,24 @@ func demoLangStatsHandleFunc(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == http.MethodPost {
 		username := r.FormValue("username")
+		langStats := getLangStats(username)
 
-		//svg := getLangStats(username)
+		// Convert data from map to struct
+		langs := []string{}
+		stats := []int{}
+		for l, s := range langStats {
+			langs = append(langs, l)
+			stats = append(stats, s)
+		}
 
-		//tmpl.Execute(w, template.HTML(svg))
+		// Prepare dataset for the chart
+		var langChart LangChart
+		langChart.Langs = langs
+		langChart.Stats = stats
+		langChart.ChartType = "bar"
+
+		tmpl.Execute(w, langChart)
 	}
-
-	//rank := getLangStats(username)
-
-	//for key, value := range rank {
-	//	fmt.Println(key, value)
-	//}
-
-	//toJSON(rank)
 }
 
 // Route: (Demo) Handle Display GitHub CtbnStats
--- langstats.go
+++ langstats.go
@@ -2,44 +2,62 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/google/go-github/v28/github"
+	"github.com/gorilla/mux"
 	"golang.org/x/oauth2"
+	"log"
+	"net/http"
 	"os"
 )
 
-// Get GitHub language rankings
-func getLangRanking(username string) map[string]int {
+// Handle Route: (API) Github LangStats (language rankings)
+func langStatsHandleFunc(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	username := vars["username"]
+
+	langStats := getLangStats(username)
+
+	// Prepare JSON to respond to the request
+	json, err := json.Marshal(langStats)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprint(w, string(json))
+}
+
+// Get GitHub language statistics
+func getLangStats(username string) map[string]int {
 	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: ""},
-	)
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: ""}, )
 
+	// Create a GitHub authenticated client
 	tc := oauth2.NewClient(ctx, ts)
-
 	client := github.NewClient(tc)
 
-	// Get a slice of
+	// Get a list of repos from GitHub account
 	repos, _, err := client.Repositories.List(ctx, username, nil)
 
 	// Address API rate limit and other errors
 	if err != nil {
-		fmt.Printf("Errors: %v\n", err)
+		fmt.Printf("Error(s): %v\n", err)
 		os.Exit(1)
 	}
 
-	// Create a list of repos
+	// Convert the list of repos to type string slice
 	var list []string
 	for _, repo := range repos {
 		list = append(list, *repo.Name)
 	}
 
-	// Get languages info for each repo
+	// Get a sum of languages in all repos
 	langStats := make(map[string]int)
 	for _, repo := range list {
 		lang, _, err := client.Repositories.ListLanguages(ctx, username, repo)
 		if err != nil {
-			fmt.Printf("Errors: %v\n", err)
+			log.Fatal(err)
 		}
 
 		for k, v := range lang {
