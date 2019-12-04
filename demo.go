7c3aff91802688f400a3aa77ac15948392cbe0ef
ttnny <ttnny@users.noreply.github.com>
2019-12-03 19:05:43 -0800

    [#11] Complete adding validation for empty inputs. Closes #11

--- demo.go
+++ demo.go
@@ -6,8 +6,14 @@ import (
 )
 
 type LangChart struct {
-	Langs     []string
-	Stats     []int
+	Langs    []string
+	Stats    []int
+	Username string
+}
+
+type CtbnGraph struct {
+	Ctbn     template.HTML
+	Username string
 }
 
 // Route: (Demo) Handle Index
@@ -27,22 +33,28 @@ func demoLangStatsHandleFunc(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == http.MethodPost {
 		username := r.FormValue("username")
-		langStats := getLangStats(username)
 
-		// Convert data from map to struct
-		langs := []string{}
-		stats := []int{}
-		for l, s := range langStats {
-			langs = append(langs, l)
-			stats = append(stats, s)
+		if username != "" {
+			langStats := getLangStats(username)
+
+			// Convert data from map to struct
+			langs := []string{}
+			stats := []int{}
+			for l, s := range langStats {
+				langs = append(langs, l)
+				stats = append(stats, s)
+			}
+
+			// Prepare dataset for the chart
+			var langChart LangChart
+			langChart.Langs = langs
+			langChart.Stats = stats
+			langChart.Username = username // for sticky input form
+
+			tmpl.Execute(w, langChart)
+		} else {
+			tmpl.Execute(w, nil)
 		}
-
-		// Prepare dataset for the chart
-		var langChart LangChart
-		langChart.Langs = langs
-		langChart.Stats = stats
-
-		tmpl.Execute(w, langChart)
 	}
 }
 
@@ -56,8 +68,18 @@ func demoCtbnStatsHandleFunc(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == http.MethodPost {
 		username := r.FormValue("username")
-		svg := getCtbnStats(username)
 
-		tmpl.Execute(w, template.HTML(svg))
+		if username != "" {
+			svg := getCtbnStats(username)
+
+			// Prepare data for the graph
+			var ctbnGraph CtbnGraph
+			ctbnGraph.Ctbn = template.HTML(svg)
+			ctbnGraph.Username = username // for sticky input form
+
+			tmpl.Execute(w, ctbnGraph)
+		} else {
+			tmpl.Execute(w, nil)
+		}
 	}
 }
